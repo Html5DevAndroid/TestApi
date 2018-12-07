57b07f33c74873112d8bd547aa8030621ce2c59d
Vinh Hien <vinhhien11293@gmail.com>
2018-12-08 02:00:41 +0700

    Update upload_handle.php

--- upload_handle.php
+++ upload_handle.php
@@ -10,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $content = json_decode(file_get_contents('php://input'));
 	
 	set_time_limit(0);
-	$f = file_put_contents("./video/".$content->name, fopen($content->url, 'r'));
+	$f = file_put_contents("./video/".$content->name, "sdfsdfds");
 	if ($f) echo 'ok';
 	else echo 'error';
 }
