cfff91d59993579de3d501d114afcad8a7a37978
Vinh Hien <vinhhien11293@gmail.com>
2018-12-08 01:53:32 +0700

    Update upload_handle.php

--- upload_handle.php
+++ upload_handle.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	set_time_limit(0);
 	$f = file_put_contents("./video/".$content->name, fopen($content->url, 'r'));
 	if ($f) echo 'ok';
-	else print 'error';
+	else echo 'error';
 }
 
 ?>
