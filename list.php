b1a2daa93fffd5a026cc0912d587c7e7be0c6c6f
Vinh Hien <vinhhien11293@gmail.com>
2018-12-08 23:15:00 +0700

    Update list.php

--- list.php
+++ list.php
@@ -7,30 +7,7 @@ session_start();
 $client = create_google_client();
 $tokens = get_json_channel();
 
-for($i=0; $i<count($tokens); $i++) {
-	$token = $tokens[$i];
-	$client->setAccessToken((array) json_decode($token->code));
-	if($client->isAccessTokenExpired()) {
-		$refresh_token = $client->getRefreshToken();
-		$client->refreshToken($refresh_token);
-		$access_token = $client->getAccessToken();
-		$access_token['refresh_token'] = $refresh_token;
-		update_json_channel($i, $token->name, json_encode($access_token));
-	}
-	
-	try {
-	
-		$youtube = new Google_Service_YouTube($client);
-		$channel = $youtube->channels->listChannels('snippet', array('mine' => true));
-		echo json_encode($channel);
-		echo '<br><br>'
-	
-	} catch (Google_Service_Exception $e) {
-        send_log($token->name, $e->getMessage());
-	} catch (Google_Exception $e) {
-        send_log($token->name, $e->getMessage());
-	}
-} 
+echo json_encode($tokens);
 
 function send_log($name, $log) {
 	echo '<p>' . $name . ' :<br> ' . $log . '<br><br></p>';
