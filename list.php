3898cd7b4aca40e7dbf3b10e0e26e3ed427b38df
Vinh Hien <vinhhien11293@gmail.com>
2018-12-08 23:19:51 +0700

    Update list.php

--- list.php
+++ list.php
@@ -7,7 +7,19 @@ session_start();
 $client = create_google_client();
 $tokens = get_json_channel();
 
-echo json_encode($tokens);
+for($i=0; $i<count($tokens); $i++) {
+	$token = $tokens[$i];
+	$client->setAccessToken((array) json_decode($token->code));
+	if($client->isAccessTokenExpired()) {
+		$refresh_token = $client->getRefreshToken();
+		$client->refreshToken($refresh_token);
+		$access_token = $client->getAccessToken();
+		$access_token['refresh_token'] = $refresh_token;
+		update_json_channel($i, $token->name, json_encode($access_token));
+	}
+	
+	echo json_encode($token);
+} 
 
 function send_log($name, $log) {
 	echo '<p>' . $name . ' :<br> ' . $log . '<br><br></p>';
