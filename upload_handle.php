acdf0c85cf8426a757a4fadba4cea0b5bfba9428
Vinh Hien <vinhhien11293@gmail.com>
2018-12-08 10:27:13 +0700

    Update upload_handle.php

--- upload_handle.php
+++ upload_handle.php
@@ -1,19 +1,17 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	/*
 	$files = glob('./video/*'); // get all file names
 	foreach($files as $file){ // iterate files
 	  if(is_file($file))
 		unlink($file); // delete file
 	}
-	*/
 	
     $content = json_decode(file_get_contents('php://input'));
 	
 	set_time_limit(0);
-	$f = file_put_contents("./video/abc.xyz", fopen($content->url, 'r'));
-	if ($f) echo file_get_contents("./video/abc.xyz");
+	$f = file_put_contents("./video/".$content->name, fopen($content->url, 'r'));
+	if ($f) echo 'ok';
 	else echo 'error';
 }
 
