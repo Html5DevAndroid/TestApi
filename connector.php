3ba5e6833020f410c42ad2cbfc3f856679caee7e
Vinh Hien <vinhhien11293@gmail.com>
2018-12-08 09:39:20 +0700

    Update connector.php

--- connector.php
+++ connector.php
@@ -1,57 +1,6 @@
 <?php
 
-$username = 'root';
-$pass = '';
-$db = 'test_db_for_youtube_api';
-
-$conn = new mysqli('localhost', $username, $pass, $db) or die("Cannot Connect");
-
-function add_token($code, $name) {
-	$conn = $GLOBALS['conn'];
-	$query = 'INSERT INTO authen_code (code, name) VALUES ("'.mysqli_real_escape_string($conn, $code).'", "'.$name.'")';
-	if($conn->query($query) === TRUE) {
-		
-	}else {
-		echo $conn->error.$query;
-	};
-}
-
-function update_token($code, $id) {
-	$conn = $GLOBALS['conn'];
-	$query = 'UPDATE authen_code SET code="'.mysqli_real_escape_string($conn, $code).'" WHERE id='.$id;
-	if($conn->query($query) === TRUE) {
-		
-	}else {
-		echo $conn->error.$query;
-	};
-}
-
-function get_token() {
-	$conn = $GLOBALS['conn'];
-	$query = 'SELECT * FROM authen_code';
-	
-	$result = $conn->query($query);
-	
-	if ($result->num_rows > 0) {
-		 $arr = array();
-		 while($row = $result->fetch_assoc()) {
-			 $object = new stdClass();
-			 $code = $row['code'];
-			 $code = (array) json_decode($code);
-			 
-			 $object->token = $code;
-			 $object->id = $row['id'];
-			 $object->name = $row['name'];
-			 
-			 $arr[] = $object;
-		 }
-		 		 
-		 return $arr;
-	} else{
-		return null;
-	}
-}
-
+/*
 function get_json_channel() {
 	$contents = get_content();
 	
@@ -89,6 +38,7 @@ function delete_json_channel($id) {
 	array_splice($contents, $id, 1);
 	put_content(json_encode($contents));
 }
+*/
 
 function get_content() {
 	return file_get_contents('https://api.myjson.com/bins/n7mim');
