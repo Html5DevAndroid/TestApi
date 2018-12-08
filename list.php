9b589fc75a5f3d243bc1664d8adee6d4f0942416
Vinh Hien <vinhhien11293@gmail.com>
2018-12-08 23:44:34 +0700

    Update list.php

--- list.php
+++ list.php
@@ -21,7 +21,7 @@ for($i=0; $i<count($tokens); $i++) {
 	try {
 	
 		$youtube = new Google_Service_YouTube($client);
-		$channel = $youtube->channels->listChannels('id', array('mine' => true));
+		$channel = $youtube->channels->listChannels('statistics', array('mine' => true));
 		echo json_encode($channel);
 		echo '<br><br>';
 	
